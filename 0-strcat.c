493a21002c2998eaed867429f06675a722ca0eef
precidev <p.ogundanaakinola@gmail.com>
2022-09-22 16:02:38 -0700

    More pointers, arrays and strings

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,51 +1,47 @@
-main.h
+#include "main.h"
 
 
-
-
-
-#ifndef MAIN_H
-
-#define MAIN_H
-
-#include <stdio.h>
 
 /**
 
- *void prototypes()
+ *_strcat - concatenates the string pointed to by @src to
 
- *int prototypes()
+ * the end of the string pointed to by @dest
 
- *char prototypes()
+ *@dest: String that will be appended
+
+ *@src: String to be concatenated upon
+
+ *
+
+ * Return: returns poiner to @dest
 
  */
 
-void reverse_array(int *a, int n);
-
-void print_number(int n);
-
-void print_buffer(char *b, int size);
-
-int _strcmp(char *s1, char *s2);
-
-char *_strcat(char *dest, char *src);
-
-char *_strncat(char *dest, char *src, int n);
-
-char *_strncpy(char *dest, char *src, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 
+char *_strcat(char *dest, char *src)
+  
+{
+  
 
-#endif /*MAIN_H*/
+  
+  int index = 0, dest_len = 0;
+  
+
+  
+  while (dest[index++])
+    
+    dest_len++;
+  
+
+  
+  for (index = 0; src[index]; index++)
+    
+    dest[dest_len++] = src[index];
+  
+
+  
+  return (dest);
+  
+}
